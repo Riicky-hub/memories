33a7c9b711b1c8638cb172f47097f295d5427550
Riicky-hub <richardjosephace1960@gmail.com>
2022-12-30 01:38:05 -0300

    Mais ajustes e toques finais -Finished version 1.0.0

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -53,7 +53,7 @@ const Form = ({ currentId, setCurrentId }) => {
         <TextField name='message' variant='outlined' fullWidth label='Message' value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name='tags' variant='outlined' fullWidth label='Tags' value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
         <div className={classes.fileInput}>
-          <FileBase type='file' multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}/>
+          <FileBase  type='file' multiple={false} onDone={({base64}) => setPostData({ ...postData, selectedFile: base64 })}/>
         </div>
         <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
         <Button variant='contained' color='secondary' size='small' onClick={clear} fullWidth>Clear</Button>
