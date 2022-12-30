7752ff1282598a4ca079e8d33c5d96c9dfa7c1ca
Riicky-hub <richardjosephace1960@gmail.com>
2022-12-30 01:39:22 -0300

    server update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,8 +5,8 @@ export const getPosts = async (req, res) => {
   try {
     const postMessages = await PostMessage.find();
     res.status(200).json(postMessages);
-  } catch (err) {
-    res.status(404).json({ message: err.message });
+  } catch (error) {
+    res.status(404).json({ message: error });
   }
 }
 
@@ -17,19 +17,41 @@ export const createPosts = async (req, res) => {
     await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(409).json({ message: error.message });
+    res.status(409).json({ message: error });
   }
 }
 
 export const updatePost = async (req, res) => {
+  const { id } = req.params;
+  const post = req.body;
   try {
-    const { id } = req.params;
-    const post = req.body;
     if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with that ${id}!`);
-
     const updatedPost = await PostMessage.findByIdAndUpdate(id, post, { new: true });
     res.json(updatedPost);
-  } catch (err) {
-    console.log(err.message);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with that ${id}!`);
+    await PostMessage.findByIdAndDelete(id);
+    res.json({ message: 'Post deleted sucessfully' });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with that ${id}!`);
+    const post = await PostMessage.findById(id);
+    const updatedPost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true });
+    res.json(updatedPost);
+  } catch (error) {
+    console.log(error);
   }
 }
