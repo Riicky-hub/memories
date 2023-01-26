d35d0d7a4e829a52dcdd2760e52df6b47a05aa86
Ricky <73661956+Riicky-hub@users.noreply.github.com>
2023-01-26 12:05:08 -0300

    Updating server url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://memories-server-3xwl.onrender.com' });
+const API = axios.create({ baseURL: 'https://memories-riickyhub.up.railway.app/' });
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
     req.headers.authorization = `Bearer ${ JSON.parse(localStorage.getItem('profile')).token }`;
